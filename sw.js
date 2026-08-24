2e59a151a6bc7a34765e581d9a61f34210cf7963
zhu21275657-creator <zhu21275657@gmail.com>
2026-08-24 16:33:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "koukou-mobile-v3";
-const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=2", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "koukou-mobile-v4";
+const ASSETS = ["./", "./index.html", "./styles.css?v=3", "./app.js?v=3", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((saved) => saved || fetch(event.request))));
