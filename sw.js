8466a65920f22d1b19156c20766ce8b01091abe1
zhu21275657-creator <zhu21275657@gmail.com>
2026-08-28 17:40:38 +0800

    expand training topics to thirty per category

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "koukou-mobile-v12";
-const ASSETS = ["./", "./index.html", "./styles.css?v=6", "./app.js?v=13", "./supabase-config.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "koukou-mobile-v13";
+const ASSETS = ["./", "./index.html", "./styles.css?v=6", "./app.js?v=14", "./supabase-config.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
